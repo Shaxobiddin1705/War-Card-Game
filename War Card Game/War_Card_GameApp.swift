3aaed354fb0cf9d7f1e12f73927f12febb26de9c
Shaxobiddin <shaxobiddins00@gmail.com>
2025-03-05 15:33:27 +0500

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Admin on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
